386b539c0ebf1efa9eb36c4d1f972cc6c224da6c
Shankar0207 <shankar.cezvatic@gmail.com>
2024-03-14 16:30:37 +0530

    chnages

--- application/views/VendorPanel/lead/detail-page.php
+++ application/views/VendorPanel/lead/detail-page.php
@@ -3,9 +3,9 @@
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
+    <!--<div class="content-header">-->
+        <!--<div class="container-fluid">-->
+            <!--<div class="row mb-2">-->
                 <!--<div class="col-sm-6">-->
                     <!--<h1 class="m-0">Query Leads Data</h1>-->
                 <!--</div> /.col -->
@@ -16,9 +16,9 @@
                 </ol>
                 </div> -->
                 <!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div>
+            <!--</div> /.row -->
+        <!--</div> /.container-fluid--> 
+    <!--</div>-->
     <!-- /.content-header -->
 
     <!-- Main content -->
