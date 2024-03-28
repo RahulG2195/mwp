35e77172778a46d2dbbbcdcc797460f206a0e21c
Shankar0207 <shankar.cezvatic@gmail.com>
2024-03-28 12:31:17 +0530

    changes

--- application/views/VendorPanel/deals/list.php
+++ application/views/VendorPanel/deals/list.php
@@ -77,7 +77,7 @@
                     </td>
                     <td><?php echo date("d M Y - h:i A", strtotime($row['created_date'])); ?> </td>
                       <td>
-                        <a class="btn btn-success" style="height: auto;margin: auto;padding: 5px 5px;" href="<?php echo $editlink.'/'.$row['deal_id']; ?>"><i class="fas fa-edit" style="font-size: 14px;">Edit</i></a></br></br>
+                        <a class="btn btn-success" style="height: auto;margin: auto;padding: 5px 5px;" href="<?php echo $editlink.'/'.$row['deal_id']; ?>"><i class="fas fa-add" style="font-size: 14px;">Edit</i></a></br></br>
                         <?php if($row['is_active']=='0'){ ?>
                             <a class="btn btn-app" style="height: auto;margin: auto;padding: 5px 5px;" href="<?php echo $activelink.'/'.$row['deal_id']; ?>"><i class="fas fa-add" style="font-size: 14px;"> Active</i></a>
                             <?php }else{ ?>
